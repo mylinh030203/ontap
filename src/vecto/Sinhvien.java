51c13c4e4337bddb4f70edbb1c7aa48291c314ee
mylinh030203 <94969882+mylinh030203@users.noreply.github.com>
2022-10-19 11:07:30 +0700

    update

--- src/vecto/Sinhvien.java
+++ src/vecto/Sinhvien.java
@@ -6,7 +6,40 @@ public class Sinhvien {
 	Scanner sc = new Scanner(System.in);
 	private String name;
 	private int age;
+	private double toan;
+	private double ly;
+	private double hoa;
 	
+	public double getToan() {
+		return toan;
+	}
+
+
+	public void setToan(double toan) {
+		this.toan = toan;
+	}
+
+
+	public double getLy() {
+		return ly;
+	}
+
+
+	public void setLy(double ly) {
+		this.ly = ly;
+	}
+
+
+	public double getHoa() {
+		return hoa;
+	}
+
+
+	public void setHoa(double hoa) {
+		this.hoa = hoa;
+	}
+
+
 	private String getName() {
 		return name;
 	}
@@ -35,7 +68,12 @@ public class Sinhvien {
 		System.out.println("Nhap tuoi");
 		this.age = sc.nextInt();
 		sc.nextLine();
-		
+		System.out.println("nhap diem toan");
+		this.toan = sc.nextDouble();
+		System.out.println("nhap diem ly");
+		this.ly = sc.nextDouble();
+		System.out.println("nhap diem hoa");
+		this.hoa = sc.nextDouble();
 	}
 	
 	
@@ -46,6 +84,12 @@ public class Sinhvien {
 //		setName(name);
 		System.out.println(" tuoi :"+this.age);
 		
+	}
+	public double diemtrungbinh() {
+		double dtb = (this.toan + this.ly + this.hoa)/3;
+		
+		
+		return dtb;
 	}
 	
 }
--- src/vecto/output.java
+++ src/vecto/output.java
@@ -4,19 +4,21 @@ public class output {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Sinhvien sv[] = new Sinhvien[2];
-//		for(int i =0 ; i<2; i++) {
-//			sv[i]= new Sinhvien();
-//			sv[i].input();
-//			
-//		}
-//		for(int i =0 ; i<2; i++) {			
-//			sv[i].output();
-//			
-//		}
-		Sinhvien sv = new Sinhvien();
-		sv.input();
-		sv.output();
+		Sinhvien sv[] = new Sinhvien[2];
+		for(int i =0 ; i<2; i++) {
+			sv[i]= new Sinhvien();
+			sv[i].input();
+			
+		}
+		for(int i =0 ; i<2; i++) {	
+	
+			sv[i].output();
+			System.out.println(sv[i].diemtrungbinh());
+			
+		}
+//		Sinhvien sv = new Sinhvien();
+//		sv.input();
+//		sv.output();
 //		
 	}
 
